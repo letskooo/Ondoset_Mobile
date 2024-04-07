e0920a7341356a3fafd79547384a0e75636665e9
KoSungmin <goghrf@KoSungminui-MacBookPro.local>
2024-04-07 11:26:36 +0900

    feat: 프로젝트 생성 및 최소 버전 설정

---
+++ Ondoset/Ondoset/OndosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  OndosetApp.swift
+//  Ondoset
+//
+//  Created by KoSungmin on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct OndosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
