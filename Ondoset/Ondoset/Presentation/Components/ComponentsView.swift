a3b57d0f1e788387ddfb89d389299c97d4bf65b8
KoSungmin <goghrf@KoSungminui-MacBookPro.local>
2024-04-10 11:41:53 +0900

    add: ComponentView 추가

--- Ondoset/Ondoset/Presentation/Components/ComponentsView.swift
+++ Ondoset/Ondoset/Presentation/Components/ComponentsView.swift
@@ -1,5 +1,5 @@
 //
-//  ComponentsExampleView.swift
+//  ComponentsView.swift
 //  Ondoset
 //
 //  Created by KoSungmin on 4/10/24.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct ComponentsExampleView: View {
+struct ComponentsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ComponentsExampleView()
+    ComponentsView()
 }
