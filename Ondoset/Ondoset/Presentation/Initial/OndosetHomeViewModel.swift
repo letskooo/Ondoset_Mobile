91f7547fa806b70e3803808d0827d2c15cd7383c
KoSungmin <goghrf@KoSungminui-MacBookPro.local>
2024-04-10 14:59:16 +0900

    feat: 하단 탭바 구현

--- Ondoset/Ondoset/Presentation/Initial/OndosetHomeViewModel.swift
+++ Ondoset/Ondoset/Presentation/Initial/OndosetHomeViewModel.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+class OndosetHomeViewModel: ObservableObject {
+    
+    // 하단 탭바 숨기기 여부
+    // false면 숨기지 않음. true면 숨김.
+    @Published var isTabBarHidden: Bool = false
+    
+}
