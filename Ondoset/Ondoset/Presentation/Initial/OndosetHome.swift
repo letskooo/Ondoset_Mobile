7ee33f9fa0942a214c36026611014acb3a7891dd
KoSungmin <goghrf@KoSungminui-MacBookPro.local>
2024-04-10 14:58:44 +0900

    add: 탭바 이미지 추가

---
+++ Ondoset/Ondoset/Presentation/Initial/OndosetHome.swift
@@ -0,0 +1,18 @@
+//
+//  OndosetHome.swift
+//  Ondoset
+//
+//  Created by KoSungmin on 4/10/24.
+//
+
+import SwiftUI
+
+struct OndosetHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OndosetHome()
+}
---
+++ Ondoset/Ondoset/Presentation/Initial/OndosetHomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OndosetHomeViewModel.swift
+//  Ondoset
+//
+//  Created by KoSungmin on 4/10/24.
+//
+
+import Foundation
