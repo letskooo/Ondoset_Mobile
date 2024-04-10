3ad886765a153723a30292b63259572ff1bc4ba1
KoSungmin <goghrf@KoSungminui-MacBookPro.local>
2024-04-11 02:03:25 +0900

    feat: 로그인뷰 UI 구현

--- Ondoset/Ondoset/Presentation/Initial/SignIn/SignInView.swift
+++ Ondoset/Ondoset/Presentation/Initial/SignIn/SignInView.swift
@@ -6,10 +6,81 @@
 //
 
 import SwiftUI
+import Combine
 
 struct SignInView: View {
+    
+    @State var idInputText: String = ""
+    @State var pwInputText: String = ""
+    @State var btnStatus: BtnStatus = .off
+    
     var body: some View {
-        Text("SignIn")
+        
+        NavigationStack {
+            VStack(spacing: 0) {
+                
+                HStack {
+                    
+                    Image("mainAppIcon")
+                    
+                    Text("ondoset")
+                        .font(Font.pretendard(.bold, size: 24))
+                        .foregroundStyle(.main)
+                }
+                .padding(.top, 35)
+                
+                Spacer()
+                
+                VStack {
+                    Image("loginIcon")
+                    
+                    TextFieldComponent(width: 340, placeholder: "아이디", inputText: $idInputText)
+                        .onChange(of: idInputText) { _ in
+                            updateBtnStatus()
+                        }
+                    
+                        
+                    TextFieldComponent(width: 340, placeholder: "비밀번호", inputText: $pwInputText)
+                        .padding(.top, 20)
+                        .onChange(of: pwInputText) { _ in
+                            updateBtnStatus()
+                        }
+           
+                    ButtonComponent(btnStatus: $btnStatus, btnText: "로그인") {
+                        
+                        // 로그인 API 호출
+                    }
+                    .padding(.top, 20)
+                    
+                    NavigationLink(destination: SignUpView()) {
+                        HStack {
+                            Text("회원가입")
+                                .font(Font.pretendard(.semibold, size: 13))
+                                .foregroundStyle(.main)
+                            
+                            Image("rightChevron")
+                        }
+                    }
+                    .padding(.top, 20)
+                }
+                .offset(y: -20)
+
+                Spacer()
+                
+                Text("Team Easy")
+                    .font(Font.pretendard(.medium, size: 16))
+                    .foregroundStyle(.darkGray)
+                    .padding(.bottom, 25)
+            }
+        }
+    }
+    
+    private func updateBtnStatus() {
+        if !idInputText.isEmpty && !pwInputText.isEmpty {
+            btnStatus = .on
+        } else {
+            btnStatus = .off
+        }
     }
 }
 
