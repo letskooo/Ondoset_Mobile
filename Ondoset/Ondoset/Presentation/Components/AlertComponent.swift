81cf92be65c7c84bf1f944825a0533f718a2463b
KoSungmin <goghrf@KoSungminui-MacBookPro.local>
2024-04-21 22:33:19 +0900

    chore: 파일 이름 수정

--- Ondoset/Ondoset/Presentation/Components/AlertComponent.swift
+++ Ondoset/Ondoset/Presentation/Components/AlertComponent.swift
@@ -1,18 +1,18 @@
 //
-//  ComponentsView.swift
+//  AlertComponent.swift
 //  Ondoset
 //
-//  Created by KoSungmin on 4/10/24.
+//  Created by KoSungmin on 4/21/24.
 //
 
 import SwiftUI
 
-struct ComponentsView: View {
+struct AlertComponent: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ComponentsView()
+    AlertComponent()
 }
