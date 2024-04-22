1edde390cbe4d2de215d596c2b4a44502a0f0d3b
KoSungmin <goghrf@KoSungminui-MacBookPro.local>
2024-04-22 17:14:26 +0900

    add: 이미지 추가

---
+++ Ondoset/Ondoset/Common/Utils/Enums.swift
@@ -0,0 +1,8 @@
+//
+//  Enums.swift
+//  Ondoset
+//
+//  Created by KoSungmin on 4/22/24.
+//
+
+import Foundation
---
+++ Ondoset/Ondoset/Presentation/Components/ClothSelectedComponent.swift
@@ -0,0 +1,18 @@
+//
+//  ClothSelectedComponent.swift
+//  Ondoset
+//
+//  Created by KoSungmin on 4/22/24.
+//
+
+import SwiftUI
+
+struct ClothSelectedComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClothSelectedComponent()
+}
