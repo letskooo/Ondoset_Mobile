a2232d1e419df589b8b6c15cd84e0ac8d09f1a3c
FpRaArNkK <mspark1184@naver.com>
2024-04-25 05:47:00 +0900

    feat: MyClothingView 작성 완료

--- Ondoset/Ondoset/Presentation/Closet/MyClothing/MyClothingView.swift
+++ Ondoset/Ondoset/Presentation/Closet/MyClothing/MyClothingView.swift
@@ -18,89 +18,110 @@ struct MyClothingView: View {
     @State var myClothingDetailedTag: (Int, String) = (-1, "")
     @State var myClothingThickness: Thickness? = nil
     @State var saveAvailable: Bool = true
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         // MARK: Main View
-        ClothingRowItemView(
-            rowTitle: "아이템 이름",
-            rowSubTitle: "15자 이내로 작성해주세요",
-            isAddImage: true,
-            content: AnyView(
-                TextFieldComponent(
-                    width: 270,
-                    placeholder: "사용자님이 알아볼만한 이름을 작성해주세요",
-                    font: .pretendard(
-                        .semibold,
-                        size: 10
-                    ),
-                    inputText: $myClothingName
+        ZStack {
+            VStack {
+                Divider()
+                ClothingRowItemView(
+                    rowTitle: "아이템 이름",
+                    rowSubTitle: "15자 이내로 작성해주세요",
+                    isAddImage: true,
+                    content: AnyView(
+                        TextFieldComponent(
+                            width: 270,
+                            placeholder: "사용자님이 알아볼만한 이름을 작성해주세요",
+                            font: .pretendard(
+                                .semibold,
+                                size: 10
+                            ),
+                            inputText: $myClothingName
+                        )
+                    )
                 )
-            )
-        )
-        Divider()
-        ClothingRowItemView(
-            rowTitle: "카테고리",
-            content: AnyView(
-                ScrollView(.horizontal) {
-                    LazyHStack(content: {
-                        ForEach(Category.allCases, id: \.self) { item in
-                            ClothTagComponent(isSelected: .constant(item == myClothingCategory), tagTitle: item.title, category: item)
-                                .onTapGesture {
-                                    myClothingCategory = item
+                Divider()
+                ClothingRowItemView(
+                    rowTitle: "카테고리",
+                    content: AnyView(
+                        ScrollView(.horizontal) {
+                            LazyHStack(content: {
+                                ForEach(Category.allCases, id: \.self) { item in
+                                    ClothTagComponent(isSelected: .constant(item == myClothingCategory), tagTitle: item.title, category: item)
+                                        .onTapGesture {
+                                            myClothingCategory = item
+                                        }
                                 }
+                            })
+                            .frame(height: 24)
+                            .padding(4)
                         }
-                    })
-                    .frame(height: 24)
-                    .padding(4)
-                }
-                    .scrollIndicators(.hidden)
-        ))
-        Divider()
-        ClothingRowItemView(
-            rowTitle: "세부 태그",
-            content: myClothingCategory != nil
-                ? AnyView(
-                    ScrollView(.horizontal) {
-                        LazyHGrid(rows: [GridItem(.flexible()), GridItem(.flexible())], alignment: .top, spacing: 10,
-                                  content: {
-                                      ForEach(DetailedTagList, id: \.0) { tag in
-                                          ClothTagComponent(isSelected: .constant(tag == myClothingDetailedTag), tagTitle: tag.1, category: myClothingCategory!)
-                                              .onTapGesture {
-                                                  myClothingDetailedTag = tag
+                            .scrollIndicators(.hidden)
+                ))
+                Divider()
+                ClothingRowItemView(
+                    rowTitle: "세부 태그",
+                    content: myClothingCategory != nil
+                        ? AnyView(
+                            ScrollView(.horizontal) {
+                                LazyHGrid(rows: [GridItem(.flexible()), GridItem(.flexible())], alignment: .top, spacing: 10,
+                                          content: {
+                                              ForEach(DetailedTagList, id: \.0) { tag in
+                                                  ClothTagComponent(isSelected: .constant(tag == myClothingDetailedTag), tagTitle: tag.1, category: myClothingCategory!)
+                                                      .onTapGesture {
+                                                          myClothingDetailedTag = tag
+                                                      }
                                               }
-                                      }
-                        })
-                        .frame(height: 60)
-                        .padding(4)
-                    }
-                        .scrollIndicators(.hidden)
-                )
-            : AnyView(
-                Text("먼저 카테고리를 선택해주세요")
-                    .font(.pretendard(.medium, size: 15))
-                    .foregroundStyle(.gray)
-            )
-        )
-        Divider()
-        ClothingRowItemView(
-            rowTitle: "두께감",
-            content: AnyView(
-                LazyHStack(alignment: .center, spacing: 50) {
-                    ForEach(Thickness.allCases, id: \.self) { item in
-                        ThicknessTagComponent(isSelected: .constant(item == myClothingThickness), thickness: item)
-                            .onTapGesture {
-                                myClothingThickness = item
+                                })
+                                .frame(height: 60)
+                                .padding(4)
                             }
-                    }
-                }
-                    .frame(width: screenWidth-40, height: 24)
-                    .padding(4)
-            )
-        )
-        Divider()
-        Spacer()
-        ButtonComponent(isBtnAvailable: $saveAvailable, width: screenWidth-40, btnText: "저장하기", radius: 15, action: { print("저장하기") })
-            .padding(.bottom, 30)
+                                .scrollIndicators(.hidden)
+                        )
+                    : AnyView(
+                        Text("먼저 카테고리를 선택해주세요")
+                            .font(.pretendard(.medium, size: 15))
+                            .foregroundStyle(.gray)
+                    )
+                )
+                Divider()
+                ClothingRowItemView(
+                    rowTitle: "두께감",
+                    content: AnyView(
+                        LazyHStack(alignment: .center, spacing: 40) {
+                            ForEach(Thickness.allCases, id: \.self) { item in
+                                ThicknessTagComponent(isSelected: .constant(item == myClothingThickness), thickness: item)
+                                    .onTapGesture {
+                                        myClothingThickness = item
+                                    }
+                            }
+                        }
+                            .frame(width: screenWidth-40, height: 24)
+                            .padding(4)
+                    )
+                )
+                Divider()
+                Spacer()
+                ButtonComponent(isBtnAvailable: $saveAvailable, width: screenWidth-40, btnText: "저장하기", radius: 15, action: { print("저장하기")
+                    dismiss()
+                })
+                    .padding(.bottom, 30)
+            }
+        }
+        .navigationTitle("나의 아이템 \(myClothing != nil ? "수정하기" : "추가하기")")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {dismiss()}, label: {
+                    Text("닫기")
+                        .font(.pretendard(.semibold, size: 15))
+                        .foregroundStyle(.gray)
+                })
+            }
+        }
+        
     }
 }
 
