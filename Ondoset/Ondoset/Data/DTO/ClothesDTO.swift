ca86d06f7612b336a4c0051b68fa383c6a3f3504
FpRaArNkK <mspark1184@naver.com>
2024-04-25 02:35:53 +0900

    chore: add more mockdata

--- Ondoset/Ondoset/Data/DTO/ClothesDTO.swift
+++ Ondoset/Ondoset/Data/DTO/ClothesDTO.swift
@@ -43,7 +43,12 @@ extension ClothesDTO {
             ClothesDTO(clothesId: 2, name: "하의1", imageURL: "image2.jpg", category: "BOTTOM", tag: "태그2", thickness: "THIN"),
             ClothesDTO(clothesId: 3, name: "아우터1", imageURL: "image3.jpg", category: "OUTER", tag: "태그3", thickness: "THICK"),
             ClothesDTO(clothesId: 4, name: "신발1", imageURL: "image4.jpg", category: "SHOE", tag: "태그4", thickness: "NORMAL"),
-            ClothesDTO(clothesId: 5, name: "악세서리1", imageURL: "image5.jpg", category: "ACC", tag: "태그5", thickness: "THIN")
+            ClothesDTO(clothesId: 5, name: "악세서리1", imageURL: "image5.jpg", category: "ACC", tag: "태그5", thickness: "THIN"),
+            ClothesDTO(clothesId: 6, name: "상의2", imageURL: "image1.jpg", category: "TOP", tag: "태그1", thickness: "NORMAL"),
+            ClothesDTO(clothesId: 7, name: "하의2", imageURL: "image2.jpg", category: "BOTTOM", tag: "태그2", thickness: "THIN"),
+            ClothesDTO(clothesId: 8, name: "아우터2", imageURL: "image3.jpg", category: "OUTER", tag: "태그3", thickness: "THICK"),
+            ClothesDTO(clothesId: 9, name: "신발2", imageURL: "image4.jpg", category: "SHOE", tag: "태그4", thickness: "NORMAL"),
+            ClothesDTO(clothesId: 10, name: "악세서리2", imageURL: "image5.jpg", category: "ACC", tag: "태그5", thickness: "THIN")
         ].compactMap { $0.toClothes() }
     }
 }
