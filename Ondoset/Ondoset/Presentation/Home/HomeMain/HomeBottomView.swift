43707a52b085439436d865c32aafccfa79705684
FpRaArNkK <mspark1184@naver.com>
2024-05-02 03:28:56 +0900

    Feat: TodaysSetupView 추가

--- Ondoset/Ondoset/Presentation/Home/HomeMain/HomeBottomView.swift
+++ Ondoset/Ondoset/Presentation/Home/HomeMain/HomeBottomView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct HomeBottomView: View {
     var body: some View {
-        Text("HomeBottom")
+        VStack(spacing: 0) {
+            HomeBottomHeaderView()
+            TodaysSetUpView()
+        }
+        .background(Color.ondosetBackground)
     }
 }
 
@@ -42,6 +46,46 @@ struct HomeBottomHeaderView: View {
     }
 }
 
+// MARK: TodaysSetUpView
+struct TodaysSetUpView: View {
+    // states
+    @State var test: Bool = true
+    
+    // property
+    let setUp: [Clothes] = [
+        .init(clothesId: 0, name: "asdf", category: .ACC, tag: "asdf", thickness: .NORMAL),
+        .init(clothesId: 1, name: "zvxc", category: .OUTER, tag: "asdf", thickness: .THIN),
+        .init(clothesId: 2, name: "aㅁㄴf", category: .BOTTOM, tag: "asdf", thickness: .NORMAL),
+        .init(clothesId: 3, name: "zvxㅁ", category: .OUTER, tag: "asdf", thickness: .THIN),
+        .init(clothesId: 4, name: "asdf", category: .TOP, tag: "asdf", thickness: .THICK),
+        .init(clothesId: 5, name: "zvㅇc", category: .SHOE, tag: "asdf", thickness: .THIN),
+        .init(clothesId: 6, name: "zvxㅁㄴ", category: .OUTER, tag: "asdf", thickness: .THIN),
+        .init(clothesId: 7, name: "asdfasdfasdff", category: .TOP, tag: "asdf", thickness: .THICK),
+        .init(clothesId: 8, name: "zvㅇㄴㄹㄴㅁㅇㄹxc", category: .SHOE, tag: "asdf", thickness: .THIN),
+    ]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHGrid(rows: [
+                GridItem(.flexible(), spacing: 10, alignment: .leading),
+                GridItem(.flexible(), spacing: 10, alignment: .leading),
+                GridItem(.flexible(), spacing: 10, alignment: .leading)
+            ], content: {
+                ForEach(setUp, id: \.clothesId) { cloth in
+                    ClothTagComponent(isSelected: $test, tagTitle: cloth.name, category: cloth.category)
+                }
+            })
+            .frame(height: 100)
+            .padding(.horizontal, 20)
+            .padding(.bottom, 20)
+        }
+        
+    }
+}
+// MARK: SetUpHistoryView
+// MARK: AIRecommendView
+// MARK: OthersOOTDView
+
 #Preview {
-    HomeBottomHeaderView()
+    HomeBottomView()
 }
