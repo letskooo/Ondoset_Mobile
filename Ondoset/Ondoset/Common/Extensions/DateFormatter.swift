3b2e3fd6fa78c4f89ab3a7f46c4e0109ff9d092b
KoSungmin <goghrf@KoSungminui-MacBookPro.local>
2024-05-03 13:03:20 +0900

    feat: DateFormatter 익스텐션 생성

--- Ondoset/Ondoset/Common/Extensions/DateFormatter.swift
+++ Ondoset/Ondoset/Common/Extensions/DateFormatter.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+extension DateFormatter {
+    
+    static let shared = DateFormatter()
+    
+    static let dateOnly: DateFormatter = {
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+    
+    static func string(epoch: Int) -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(epoch))
+        
+        return DateFormatter.dateOnly.string(from: date)
+    }
+}
