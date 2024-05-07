339f5987f2517c865539f5e86e04eeb8519188c4
KoSungmin <goghrf@KoSungminui-MacBookPro.local>
2024-05-07 22:00:33 +0900

    feat: ClothEndPoint 작성 완료

---
+++ Ondoset/Ondoset/Presentation/Coordi/AddCoordi/AddCoordiPlanView.swift
@@ -0,0 +1,18 @@
+//
+//  AddCoordiPlanView.swift
+//  Ondoset
+//
+//  Created by KoSungmin on 5/7/24.
+//
+
+import SwiftUI
+
+struct AddCoordiPlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCoordiPlanView()
+}
