42b130cb58c698e2d6dbe1d13bd2a36f80c2cc06
FpRaArNkK <mspark1184@naver.com>
2024-05-09 19:50:22 +0900

    Feat: getMyAllClothes 연결

--- Ondoset/Ondoset/Presentation/Closet/ClosetMain/ClosetMainViewModel.swift
+++ Ondoset/Ondoset/Presentation/Closet/ClosetMain/ClosetMainViewModel.swift
@@ -9,8 +9,37 @@ import Foundation
 
 final class ClosetMainViewModel: ObservableObject {
     
+    private let clothesUseCase: ClothesUseCase = ClothesUseCase.shared
+    
     @Published var selectedTab: Int = 0
     @Published var searchText: String = ""
     @Published var clothesData: [Clothes] = []
     
+    private var clothesLastPage: Int = -1
+    
+
+    
+    init() {
+        Task {
+            await getMyAllClothes()
+        }
+    }
+    
+    private func getMyAllClothes() async {
+        guard clothesLastPage != -2 else { return }
+        
+        if let result = await clothesUseCase.getAllClothes(lastPage: clothesLastPage) {
+            
+            self.clothesLastPage = result.lastPage
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.clothesData = result.ClothesList
+            }
+            
+        }
+    }
+    
+    private func getMyClothes(by category: Category) async {
+        
+    }
 }
