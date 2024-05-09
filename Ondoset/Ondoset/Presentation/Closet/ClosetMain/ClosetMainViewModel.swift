6ed8b38252b1828a2e665485a6310ef9e1e07b1b
FpRaArNkK <mspark1184@naver.com>
2024-05-09 18:53:14 +0900

    Feat: closetMainVM init

--- Ondoset/Ondoset/Presentation/Closet/ClosetMain/ClosetMainViewModel.swift
+++ Ondoset/Ondoset/Presentation/Closet/ClosetMain/ClosetMainViewModel.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+final class ClosetMainViewModel: ObservableObject {
+    
+    @Published var selectedTab: Int = 0
+    @Published var searchText: String = ""
+    @Published var clothesData: [Clothes] = []
+    
+}
