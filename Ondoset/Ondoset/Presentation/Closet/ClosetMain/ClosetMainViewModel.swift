3c4e449ca78122747332a88fe7f3fdba11cf85bc
FpRaArNkK <mspark1184@naver.com>
2024-05-09 20:34:45 +0900

    chore: 코드 스타일 수정

--- Ondoset/Ondoset/Presentation/Closet/ClosetMain/ClosetMainViewModel.swift
+++ Ondoset/Ondoset/Presentation/Closet/ClosetMain/ClosetMainViewModel.swift
@@ -11,20 +11,19 @@ final class ClosetMainViewModel: ObservableObject {
     
     private let clothesUseCase: ClothesUseCase = ClothesUseCase.shared
     
+    /// 선택된 탭 넘버
     @Published var selectedTab: Int = 0 {
         // 값이 변경됨에 따라 해당 함수가 호출됩니다.
         didSet {
-            self.clothesLastPage = -1
+            self.clothesLastPage = -1 // 탭 변경 시엔 항상 -1로 시작
             Task {
-                if selectedTab != 0 {
-                    await getMyClothes(by: Category.allCases[selectedTab - 1])
-                }
-                else {
-                    await getMyAllClothes()
-                }
+                selectedTab != 0
+                ? await getMyClothes(by: Category.allCases[selectedTab - 1])
+                : await getMyAllClothes()
             }
         }
     }
+    /// 검색 텍스트
     @Published var searchText: String = "" {
         didSet { searchClothes(by: self.searchText) }
     }
@@ -32,15 +31,13 @@ final class ClosetMainViewModel: ObservableObject {
     @Published var presentingClothesData: [Clothes] = []
     /// 뒤(뷰모델)에서 관리되는 Data
     private var clothesData: [Clothes] = [] {
-        didSet { setNewPresenting() }
+        didSet { setPresentingData() }
     }
-    
+    /// clothes Data last page
     private var clothesLastPage: Int = -1
         
     init() {
-        Task {
-            await getMyAllClothes()
-        }
+        Task { await getMyAllClothes() }
     }
 }
 
@@ -51,6 +48,7 @@ extension ClosetMainViewModel {
 // MARK: Internal Functions
 extension ClosetMainViewModel {
     
+    /// 전체 clothes를 가져옵니다 by API
     private func getMyAllClothes() async {
          guard clothesLastPage != -2 else { return }
          
@@ -59,6 +57,7 @@ extension ClosetMainViewModel {
          }
      }
      
+    /// 카테고리 값으로 clothes를 가져옵니다 by API
     private func getMyClothes(by category: Category) async {
          guard selectedTab != 0, clothesLastPage != -2 else { return }
          
@@ -67,6 +66,7 @@ extension ClosetMainViewModel {
          }
      }
     
+    /// API에서 받은 Data를 clothesData에 전달합니다.
     private func setReceivedData(clothesList: [Clothes], lastPage: Int) {
         self.clothesLastPage = lastPage
         DispatchQueue.main.async { [weak self] in
@@ -75,16 +75,22 @@ extension ClosetMainViewModel {
         }
     }
     
-    private func setNewPresenting() {
+    /// 새로 받은 clothesData를 화면에 표시합니다
+    private func setPresentingData() {
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
-            self.searchText = ""
+            self.searchText = "" // 새로운 화면이므로 텍스트 필드가 공백
             self.presentingClothesData = self.clothesData
         }
     }
     
+    /// 검색 값에 따른 Clothes 표시 데이터를 전달합니다.
     private func searchClothes(by text: String) {
-        guard !text.isEmpty else { return }
+        guard !text.isEmpty else {
+            // 텍스트 비면 전체 데이터 표시
+            self.presentingClothesData = self.clothesData
+            return
+        }
         self.presentingClothesData = self.clothesData.filter({ $0.name.contains(text) })
     }
 }
