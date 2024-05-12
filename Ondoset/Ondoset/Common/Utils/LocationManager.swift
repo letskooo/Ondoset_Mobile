2151cb064b001e8e744a418d37675430c4ca19ce
KoSungmin <goghrf@KoSungminui-MacBookPro.local>
2024-05-12 16:07:24 +0900

    feat: LocationManager 구현

--- Ondoset/Ondoset/Common/Utils/LocationManager.swift
+++ Ondoset/Ondoset/Common/Utils/LocationManager.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    private let locationManager = CLLocationManager()
+    @Published var authorizationStatus: CLAuthorizationStatus
+    
+    override init() {
+        
+        self.authorizationStatus = locationManager.authorizationStatus
+        super.init()
+        locationManager.delegate = self
+    }
+    
+    func requestPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        self.authorizationStatus = manager.authorizationStatus
+    }
+}
