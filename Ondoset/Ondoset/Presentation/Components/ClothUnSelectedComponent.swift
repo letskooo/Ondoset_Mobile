d6617f68e5d28dd33027ab3cfded54c90902cf11
FpRaArNkK <mspark1184@naver.com>
2024-05-12 22:55:42 +0900

    Feat: ClothUnSelectedComponenet ViewModel 추가

--- Ondoset/Ondoset/Presentation/Components/ClothUnSelectedComponent.swift
+++ Ondoset/Ondoset/Presentation/Components/ClothUnSelectedComponent.swift
@@ -15,17 +15,9 @@ struct ClothUnSelectedComponent: View {
     
     /// 서치모드
     @Binding var searchMode: Bool
-    /// 선택 검색 탭
-    @State var selectedTab: Int = 0
-    /// 검색 텍스트
-    @State var searchText: String = ""
-    /// 검색 옷 목록
-    @State var clothesList: [Clothes] = []
     
-    // 옷 카테고리
-//    let category: Category
-    // 옷 이름
-//    let clothName: String
+    @StateObject var clothSearchVM : ClothSearchViewModel = .init()
+    
     // 너비
     let width: CGFloat
     // 우측에 들어가는 내용
@@ -142,21 +134,21 @@ struct ClothUnSelectedComponent: View {
                             .padding(.horizontal)
                             
                             // 중간 세그먼트 컨트롤
-                            SegmentControlComponent(selectedTab: $selectedTab, tabMenus: MyClosetTab.allCases.map{ $0.rawValue }, isMain: false)
+                            SegmentControlComponent(selectedTab: $clothSearchVM.selectedTab, tabMenus: MyClosetTab.allCases.map{ $0.rawValue }, isMain: false)
                                 
                             // 중간 서치바
-                            SearchBarComponent(searchText: $searchText, placeHolder: "등록한 옷을 검색하세요", searchAction: { print($0) })
+                            SearchBarComponent(searchText: $clothSearchVM.searchText, placeHolder: "등록한 옷을 검색하세요", searchAction: { print($0) })
                                 .padding(.horizontal)
                             
                             // 아이템 목록
                             ScrollView(.vertical) {
                                 VStack(spacing: 10) {
-                                    ForEach(test.indices, id: \.self) { index in
+                                    ForEach(clothSearchVM.presentingClothesData.indices, id: \.self) { index in
                                         ClothSelectedComponent(
-                                            category: test[index].category,
-                                            clothName: test[index].name,
-                                            clothTag: test[index].tag,
-                                            clothThickness: test[index].thickness ?? .NORMAL,
+                                            category: clothSearchVM.presentingClothesData[index].category,
+                                            clothName: clothSearchVM.presentingClothesData[index].name,
+                                            clothTag: clothSearchVM.presentingClothesData[index].tag,
+                                            clothThickness: clothSearchVM.presentingClothesData[index].thickness ?? .NORMAL,
                                             width: 300
                                         )
                                         .onTapGesture {
@@ -183,6 +175,98 @@ struct ClothUnSelectedComponent: View {
                     }
                 }
             }
+    }
+}
+
+final class ClothSearchViewModel: ObservableObject {
+    
+    private let clothesUseCase: ClothesUseCase = ClothesUseCase.shared
+    
+    /// MyClothingView 표시
+    @Published var presentMyClothing: Bool = false
+    /// 선택한 옷
+    @Published var myClothing: Clothes? = nil
+    /// 선택된 탭 넘버
+    @Published var selectedTab: Int = 0 {
+        // 값이 변경됨에 따라 해당 함수가 호출됩니다.
+        didSet {
+            self.clothesLastPage = -1 // 탭 변경 시엔 항상 -1로 시작
+            Task {
+                selectedTab != 0
+                ? await getMyClothes(by: Category.allCases[selectedTab - 1])
+                : await getMyAllClothes()
+            }
+        }
+    }
+    /// 검색 텍스트
+    @Published var searchText: String = "" {
+        didSet { searchClothes(by: self.searchText) }
+    }
+    /// 화면에 표시되는 Data
+    @Published var presentingClothesData: [Clothes] = []
+    /// 뒤(뷰모델)에서 관리되는 Data
+    private var clothesData: [Clothes] = [] {
+        didSet { setPresentingData() }
+    }
+    /// clothes Data last page
+    private var clothesLastPage: Int = -1
+    
+    init() {
+        Task { await getMyAllClothes() }
+    }
+}
+
+// MARK: Interface Functions
+extension ClothSearchViewModel {
+}
+
+// MARK: Internal Functions
+extension ClothSearchViewModel {
+    
+    /// 전체 clothes를 가져옵니다 by API
+    private func getMyAllClothes() async {
+         guard clothesLastPage != -2 else { return }
+         
+         if let result = await clothesUseCase.getAllClothes(lastPage: clothesLastPage) {
+             setReceivedData(clothesList: result.ClothesList, lastPage: result.lastPage)
+         }
+     }
+     
+    /// 카테고리 값으로 clothes를 가져옵니다 by API
+    private func getMyClothes(by category: Category) async {
+         guard selectedTab != 0, clothesLastPage != -2 else { return }
+         
+         if let result = await clothesUseCase.getAllClothesByCategory(getAllClothesByCategoryDTO: .init(category: category.rawValue, lastPage: clothesLastPage)) {
+             setReceivedData(clothesList: result.clothesList, lastPage: result.lastPage)
+         }
+     }
+    
+    /// API에서 받은 Data를 clothesData에 전달합니다.
+    private func setReceivedData(clothesList: [Clothes], lastPage: Int) {
+        self.clothesLastPage = lastPage
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            self.clothesData = clothesList
+        }
+    }
+    
+    /// 새로 받은 clothesData를 화면에 표시합니다
+    private func setPresentingData() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            self.searchText = "" // 새로운 화면이므로 텍스트 필드가 공백
+            self.presentingClothesData = self.clothesData
+        }
+    }
+    
+    /// 검색 값에 따른 Clothes 표시 데이터를 전달합니다.
+    private func searchClothes(by text: String) {
+        guard !text.isEmpty else {
+            // 텍스트 비면 전체 데이터 표시
+            self.presentingClothesData = self.clothesData
+            return
+        }
+        self.presentingClothesData = self.clothesData.filter({ $0.name.contains(text) })
     }
 }
 
